e0a4ed2ceff658aba2f0859f4b913f327720a4fc
ctqwa <ctqwaa@gmail.com>
2026-01-05 00:44:10 +0500

    Add commented memory leak fix - usage counting
    Added incUsage()/decUsage() calls in streamdata constructor/destructor
    but COMMENTED OUT per user request. Uncomment to enable reference
    counting and prevent memory leak from SensorGlucoseData objects.
    
    To enable: Remove // comments on lines with incUsage/decUsage

--- Common/src/main/cpp/streamdata.hpp
+++ Common/src/main/cpp/streamdata.hpp
@@ -4,40 +4,50 @@
 extern Sensoren *sensors;
 
 struct streamdata {
-	int libreversion;
-	int sensorindex;
-	SensorGlucoseData *hist;
-	streamdata(int libreversion,int sensorindex,SensorGlucoseData *sens):libreversion(libreversion),sensorindex(sensorindex),hist(sens) {}
-	streamdata(int libreversion,int sensorindex):streamdata(libreversion,sensorindex,sensors->getSensorData(sensorindex)) {}
-	streamdata(int libreversion,const char *sensorname):streamdata(libreversion,sensors->sensorindex(sensorname)) {}
-	virtual bool good() const {
-		return true;
-		};
-	virtual ~streamdata() {};
-
-	};
-struct libre3stream:streamdata {
-	libre3stream(int sensindex,SensorGlucoseData *sens): streamdata(3, sensindex,sens){};
-	};
+  int libreversion;
+  int sensorindex;
+  SensorGlucoseData *hist;
+  streamdata(int libreversion, int sensorindex, SensorGlucoseData *sens)
+      : libreversion(libreversion), sensorindex(sensorindex), hist(sens) {
+    // MEMORY LEAK FIX: Uncomment to enable reference counting
+    // if (hist) hist->incUsage();
+  }
+  streamdata(int libreversion, int sensorindex)
+      : streamdata(libreversion, sensorindex,
+                   sensors->getSensorData(sensorindex)) {}
+  streamdata(int libreversion, const char *sensorname)
+      : streamdata(libreversion, sensors->sensorindex(sensorname)) {}
+  virtual bool good() const { return true; };
+  virtual ~streamdata() {
+    // MEMORY LEAK FIX: Uncomment to enable reference counting
+    // if (hist) hist->decUsage();
+  };
+};
+struct libre3stream : streamdata {
+  libre3stream(int sensindex, SensorGlucoseData *sens)
+      : streamdata(3, sensindex, sens) {};
+};
 #ifdef DEXCOM
-struct accustream:streamdata {
-	accustream(int sensindex,SensorGlucoseData *sens): streamdata(0x20, sensindex,sens){};
-	};
+struct accustream : streamdata {
+  accustream(int sensindex, SensorGlucoseData *sens)
+      : streamdata(0x20, sensindex, sens) {};
+};
 #endif
 #ifdef SIBIONICS
 #include "sibionics/SiContext.hpp"
-struct sistream:streamdata {
-	SiContext sicontext;
-	sistream(int sensindex,SensorGlucoseData *sens): streamdata(0x10, sensindex,sens),sicontext(sens){ };
-	};
+struct sistream : streamdata {
+  SiContext sicontext;
+  sistream(int sensindex, SensorGlucoseData *sens)
+      : streamdata(0x10, sensindex, sens), sicontext(sens) {};
+};
 #endif
 
 #ifdef DEXCOM
 #include "dexcom/DexContext.hpp"
-struct dexcomstream:streamdata {
-   
+struct dexcomstream : streamdata {
 
-	DexContext dexcontext;
-	dexcomstream(int sensindex,SensorGlucoseData *sens): streamdata(0x40, sensindex,sens),dexcontext(sens){};
-	};
-#endif 
+  DexContext dexcontext;
+  dexcomstream(int sensindex, SensorGlucoseData *sens)
+      : streamdata(0x40, sensindex, sens), dexcontext(sens) {};
+};
+#endif
